c39ef8ed7c5bfe08840264d797dd6b9fac555f1d
Oleg <olegsv3007@yandex.ru>
2017-04-01 23:25:44 +0300

    Beta

--- FileWork/FileWorker.cs
+++ FileWork/FileWorker.cs
@@ -16,32 +16,98 @@ namespace FileWork
         static private StreamReader _reader;
         static private StreamWriter _writer;
 
-        static private List<DataItem> _list;
+        static private List<string> _list;
+        static private List<DataItem> _ListData;
 
         static public string ActiveLanguage { get; set; }
+        static public string ActiveCategory { get; set; }
 
         static public void Start()
         {
+            File.Create(_fileName).Dispose();
             UpdateList();
         }
 
         static public void UpdateList()
         {
-            _list = new List<DataItem>();
+            _list = new List<string>();
+            _ListData = new List<DataItem>();
+
             string line = string.Empty;
 
             _reader = new StreamReader(_fileName);
 
             while ((line = _reader.ReadLine()) != null)
             {
-                
+                _list.Add(line);
             }
             _reader.Close();
+
+            foreach(var str in _list)
+            {
+                string[] tmp = str.Split('|');
+                var dataItem = new DataItem() { Language = tmp.Length > 0 ? tmp[0] : "",
+                                                Category = tmp.Length > 1 ? tmp[1] : "",
+                                                Word1 = tmp.Length > 2 ? tmp[2] : "",
+                                                Word2 = tmp.Length > 3 ? tmp[3] : "" };
+                _ListData.Add(dataItem);
+            }
         }
 
         static public List<string> GetLanguages()
         {
-
+            return (from lang in _ListData
+                    group lang by lang.Language
+                    into tmp
+                    select tmp.Key).ToList();
         }        
+
+        static public bool AddLanguage(string lang)
+        {
+            if (GetLanguages().Contains(lang)) return false;
+            _writer = new StreamWriter(_fileName, true);
+            _writer.WriteLine(lang);
+            _writer.Close();
+            UpdateList();
+            return true;
+        }
+
+        static public bool AddCategory(string cat)
+        {
+            var formatString = $"{ActiveLanguage}|{cat}";
+            if (_list.Contains(formatString)) return false;
+            _writer = new StreamWriter(_fileName, true);
+            _writer.WriteLine(formatString);
+            _writer.Close();
+            UpdateList();
+            return true;
+        }
+
+        static public List<string> GetCategories()
+        {
+            return (from cat in _ListData
+                    where cat.Language == ActiveLanguage
+                    group cat by cat.Category into tmp
+                    select tmp.Key).ToList<string>();
+        }
+
+        static public bool AddWords(string word1, string word2)
+        {
+            string formatString = $"{ActiveLanguage}|{ActiveCategory}|{word1}|{word2}";
+
+            _writer = new StreamWriter(_fileName, true);
+            _writer.WriteLine(formatString);
+            _writer.Close();
+
+            UpdateList();
+            return true;
+        }
+
+        static public List<Card> GetWords()
+        {
+            return (from card in _ListData
+                    where card.Language == ActiveLanguage && card.Category == ActiveCategory
+                    select new Card { Word1 = card.Word1, Word2 = card.Word2 }).ToList<Card>();
+        }
     }
 }
--- FileWork/Program.cs
+++ FileWork/Program.cs
@@ -13,8 +13,39 @@ namespace FileWork
             FileWorker.Start();
             FileWorker.AddLanguage("English");
             FileWorker.AddLanguage("French");
-            FileWorker.ActiveLanguage = "French";
+            FileWorker.ActiveLanguage = "English";
+
+            foreach(var i in FileWorker.GetLanguages())
+            {
+                Console.WriteLine(i);
+            }
             FileWorker.AddCategory("Flat");
+            FileWorker.AddCategory("Job");
+            FileWorker.AddCategory("Hobby");
+
+            FileWorker.ActiveLanguage = "French";
+
+            FileWorker.AddCategory("Paris");
+            FileWorker.AddCategory("Nature");
+            FileWorker.AddCategory("Fruits");
+
+            FileWorker.ActiveLanguage = "English";
+
+            foreach (var i in FileWorker.GetCategories())
+            {
+                Console.WriteLine(i);
+            }
+
+            FileWorker.ActiveCategory = "Job";
+
+            FileWorker.AddWords("Head", "Директор");
+            FileWorker.AddWords("Office", "Офис");
+            FileWorker.AddWords("Meeting", "Встреча");
+
+            foreach (var i in FileWorker.GetWords())
+            {
+                Console.WriteLine($"{i.Word1}\t{i.Word2}");
+            }
         }
     }
 }
